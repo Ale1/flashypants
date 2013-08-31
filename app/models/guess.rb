75a01aad29d0c876d841fe0c98e25531eaf5ecb3
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 17:01:24 -0700

    model methods and round model

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,18 @@
 class Guess < ActiveRecord::Base
   belongs_to :card,:round
+
+  def solved?
+    self.solved
+  end
+
+  def solve
+    #NOTE: only solves, does no unsolve.
+    self.solved = 1
+
+  def unsolve
+  self.solved = 0 
+  end
+
+
+
 end
