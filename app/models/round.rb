f7971c3ff15d555c50ea984d454ca0a04cb032e5
danapczynski <apczynski@gmail.com>
2013-08-30 19:05:16 -0700

    fixed typo in helper file

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,13 +1,16 @@
 class Round < ActiveRecord::Base
   
-  def all_round_cards
+  # return all cards from the current round.
+  def all_round_cards 
     Deck.find(self.deck_id).all_cards
   end
 
+  # return only unsolved cards.
   def round_unsolved_cards
     Guess.where('deck_id = ?', self.deck_id && 'status = ?','0')
   end
 
+  # return only solved cards.
   def round_solved_cards
     self.all_round_cards - self.round_unsolved_cards
   end
