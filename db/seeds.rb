599d4164ac3afb8011109b36c0eeee90239d5cac
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 19:21:53 -0700

    basic seeding

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+#test deck:
+
+Deck.create(title:'Animals')
+Card.create(prompt:'goes moo',answer:'cow', deck_id:1)
+Card.create(prompt:'goes quack',answer:'duck',deck_id:1)
+Card.create(prompt:'goes meow',answer:'cat',deck_id:1)
