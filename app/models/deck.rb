3867df84b3cf810b10b4f28e4aaffe042dc926e3
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 18:32:29 -0700

    round method

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,5 @@
 class Deck < ActiveRecord::Base
-  has_many :card
+  has_many :cards
 
   def all_cards
     #checks how many cards in this deck
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,6 +1,8 @@
 class Round < ActiveRecord::Base
-  def unsolved_cards
+  
+  def unsolved_cards  #should go into Round model?
     round_deck = self.deck_id
     Card.where(deck_id == round_deck && ) #unfinished
+  end
 
 end
