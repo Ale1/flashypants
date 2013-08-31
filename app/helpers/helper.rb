07509f65a2a3e42fbc8b48983ecc8b6b7229606a
danapczynski <apczynski@gmail.com>
2013-08-30 19:06:13 -0700

    fixed typo in helper file

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,16 +1,28 @@
 helpers do
 
-  def login
-    session[:user_id] = self.id
+  def login(user_id)
+    session[:user_id] = User.find(id: user_id)
   end
 
   def logout
     session[:user_id].clear
   end
 
+  def logged_in?
+    if session[:user_id] ? true : false
+  end
+
+  def current_user
+    if logged_in? ? User.find(session[:user_id]) : nil
+  end
+
+  def current_user_id
+    if logged_in? ? current_user.id : nil
+  end
+
   def start_round(deck) # Luisa, use this when player picks a deck!
     session[:round_id] = Round.create(deck)
   end
 
-  
 end
+
