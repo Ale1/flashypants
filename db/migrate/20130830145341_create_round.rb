1083a850b4d9cc4743f973bc8f44fd00795873ee
Luisa <Emmi.Beck@gmail.com>
2013-09-01 12:03:28 -0700

    fixed Luisa's merge conflicts

--- db/migrate/20130830145341_create_round.rb
+++ db/migrate/20130830145341_create_round.rb
@@ -3,10 +3,7 @@ class CreateRound < ActiveRecord::Migration
     create_table :rounds do |t|
       t.integer :user_id
       t.integer :score, default: 0
-<<<<<<< HEAD
       t.integer :attempts, default: 0
-=======
->>>>>>> merging Dan't changes
       t.integer :deck_id
     end
   end
